aae94a7817af26aa902c13d2a745b78383bfaabf
rammy427 <grex4274@gmail.com>
2025-11-16 19:48:59 -0400

    Donations view now requires login.

--- sheltr/donations.py
+++ sheltr/donations.py
@@ -1,6 +1,8 @@
 from flask import (Blueprint, render_template)
+from sheltr.auth import login_required
 bp = Blueprint('donations', __name__, url_prefix='/donations')
 
 @bp.route('/')
+@login_required
 def view():
     return render_template('donations.html')
