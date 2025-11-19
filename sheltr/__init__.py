5daddf386afe2c0d776c43c238bb03169a66e883
gaba-alemana <gabriela.german@upr.edu>
2025-11-19 18:30:48 -0400

    Little of Emergency

--- sheltr/__init__.py
+++ sheltr/__init__.py
@@ -37,4 +37,7 @@ def create_app(test_config=None):
     from . import auth
     app.register_blueprint(auth.bp)
     
+    from . import emergency
+    app.register_blueprint(emergency.bp)
+    
     return app
--- sheltr/emergency.py
+++ sheltr/emergency.py
@@ -1,1 +1,9 @@
-# Emergency thingie
+from flask import (Blueprint, render_template)
+from sheltr.auth import login_required
+bp = Blueprint('emergency', __name__, url_prefix = '/emergency')
+
+@bp.route('/')
+@login_required
+
+def view():
+    return render_template('emergency.html')
