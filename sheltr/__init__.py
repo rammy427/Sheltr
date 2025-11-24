0591223069f9007f7d5c23d38ae3308f345214de
ianperez5 <ian.perez5@upr.edu>
2025-11-24 09:30:06 -0400

    Add ignition script for one-command server launch and fix home route redirect

--- sheltr/__init__.py
+++ sheltr/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import (Flask, render_template)
+from flask import (Flask, g, redirect, render_template, url_for)
 from flask_bootstrap import Bootstrap5
 
 def create_app(test_config=None):
@@ -27,15 +27,17 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    # A simple page that says hello
-    @app.route('/')
-    def index():
-        return render_template('index.html')
-    
     from . import db
     db.init_app(app)
 
     from . import auth
     app.register_blueprint(auth.bp)
-    
+
+    # Redirect to login if not authenticated, otherwise show home
+    @app.route('/')
+    def index():
+        if not hasattr(g, 'user') or g.user is None:
+            return redirect(url_for('auth.login'))
+        return render_template('index.html')
+
     return app
