8f2de6e9afca518a18c5f3a60ade0a609d38f948
rammy427 <grex4274@gmail.com>
2025-11-29 20:33:53 -0400

    Filtering tasks by status in controller.

--- sheltr/donations.py
+++ sheltr/donations.py
@@ -1,4 +1,4 @@
-from flask import (Blueprint, g, render_template)
+from flask import (Blueprint, g, render_template, request)
 from sheltr.auth import login_required
 from sheltr.models import Volunteer
 bp = Blueprint('donations', __name__, url_prefix='/donations')
@@ -8,4 +8,10 @@ bp = Blueprint('donations', __name__, url_prefix='/donations')
 def view():
     # Get all the current tasks assigned to the user.
     tasks = Volunteer.get_by_username(g.user.username).get_tasks()
+
+    # Filter tasks by status if specified.
+    status = request.args.get("status")
+    if status:
+        tasks = [t for t in tasks if t.state == status]
+
     return render_template('donations.html', tasks=tasks)
