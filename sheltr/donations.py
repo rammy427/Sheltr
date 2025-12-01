b3dd0142978f4f37a4650f67a58b9da8d6734c87
rammy427 <grex4274@gmail.com>
2025-11-30 22:29:17 -0400

    Implemented buttons to change task statuses.

--- sheltr/donations.py
+++ sheltr/donations.py
@@ -1,6 +1,6 @@
-from flask import (Blueprint, g, render_template, request)
+from flask import (Blueprint, g, render_template, request, jsonify)
 from sheltr.auth import login_required
-from sheltr.models import Volunteer
+from sheltr.models import Volunteer, Task
 bp = Blueprint('donations', __name__, url_prefix='/donations')
 
 @bp.route('/')
@@ -15,3 +15,17 @@ def view():
         tasks = [t for t in tasks if t.status in status]
 
     return render_template('donations.html', tasks=tasks, status=status)
+
+@bp.route('/update_status', methods=['POST'])
+@login_required
+def update_status():
+    data = request.get_json()
+    task_id = data.get("id")
+    new_status = data.get("status")
+
+    task = Task.get_by_id(task_id)
+    if not task:
+        return jsonify(success=False, error="Task not found")
+    
+    task.update_status(new_status)
+    return jsonify(success=True)
