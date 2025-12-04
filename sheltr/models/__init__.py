96d2c8fdd3a8a2045d741e2b46c0652e0322069a
rammy427 <grex4274@gmail.com>
2025-12-04 15:10:15 -0400

    Minor change in __init__.py.

--- sheltr/models/__init__.py
+++ sheltr/models/__init__.py
@@ -7,8 +7,6 @@ from .user import User
 from .volunteer import Volunteer
 from .manager import Manager
 from .emergency import Emergency
-
-__all__ = ['User', 'Volunteer', 'Manager', 'Emergency']
 from .task import Task
 
-__all__ = ['User', 'Volunteer', 'Manager', 'Task']
+__all__ = ['User', 'Volunteer', 'Manager', 'Emergency', 'Task']
