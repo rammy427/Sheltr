37c0b33e397de3e347c1c66e6f78f52fa503cfe6
Miguel-HernandezB <54182236+Miguel-HernandezB@users.noreply.github.com>
2025-12-08 15:43:23 -0400

    added inserts to test donations

--- sheltr/donations.py
+++ sheltr/donations.py
@@ -11,32 +11,43 @@ def view():
     """View the 10 most recent donations"""
     db = get_db()
 
-    donation_list = db.execute('SELECT * FROM donation ORDER BY donation_date DESC LIMIT 10').fetchall()
+    donation_list = db.execute('SELECT  FROM donation ORDER BY donation_date DESC LIMIT 10').fetchall()
 
     return render_template('donations/donations.html', donations = donation_list)
+
+
+"""
 
 @bp.route('/make-donation', methods = ('GET', 'POST'))
 @login_required
 def make_donation():
     if request.method == 'POST':
-        emergency_selection = request.form['Select Emergency']
-        amount = request.form['Donation amount']
-        using = request.form['Select payment service']
-        msg = request.form['Write a message']
+        emergency_selection = request.form.get['emergency_id']
+        amount = request.form.get['amount']
+        provider = request.form.get['provider']
+        msg = request.form.get['msg']
         error = None
 
         if not emergency_selection:
             error = 'Required: Select emergency to donate'
         if not amount:
             error = 'Required: Select an amount to donate'
-        if not using:
+        if not provider:
             error = 'Required: Select payment service provider'
         
         if error is not None:
             flash(error)
         else:
             db = get_db()
-            db.execute('INSERT INTO donation (emergency_id, user_id, donation_quantity, donation_message)')
+            db.execute('INSERT INTO donation (emergency_id, user_id, donation_quantity, donation_message) VALUES(?, ?, ?, ?)', (emergency_selection, g.user['id'], amount, msg))
+
+            db.commit()
+            return(redirect(url_for('donations.view')))
+        
+    return render_template('donations/make-donation.html')
+
+
+"""
 
 
     
