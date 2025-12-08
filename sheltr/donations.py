93e12123e36157ca1daf3cba3d9e23c178726d48
Miguel-HernandezB <54182236+Miguel-HernandezB@users.noreply.github.com>
2025-12-07 23:10:31 -0400

    added small modifications to file

--- sheltr/donations.py
+++ sheltr/donations.py
@@ -1,8 +1,14 @@
 from flask import (Blueprint, render_template)
 from sheltr.auth import login_required
+from  sheltr.db import get_db
+
 bp = Blueprint('donations', __name__, url_prefix='/donations')
 
 @bp.route('/')
 @login_required
 def view():
+    db = get_db()
+
     return render_template('donations.html')
+
+@bp.route('/donate')
