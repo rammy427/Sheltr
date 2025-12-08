0f125ff9b040cd4543744c55ef64e91ffb2db5c2
rammy427 <grex4274@gmail.com>
2025-12-07 21:44:42 -0400

    Added new shelter model methods.

--- sheltr/models/shelter.py
+++ sheltr/models/shelter.py
@@ -4,6 +4,7 @@ Handles all shelter database operations.
 """
 
 from sheltr.db import get_db
+from .task import Task
 
 class Shelter:
     def __init__(self, id=None, name=None, location=None, description=None):
@@ -11,6 +12,7 @@ class Shelter:
         self.name = name
         self.location = location
         self.description = description
+        self.tasks = []
 
     @classmethod
     def get_all(cls):
@@ -20,6 +22,24 @@ class Shelter:
         if rows is None:
             return None
         return [cls._from_db_row(row) for row in rows]
+    
+    @classmethod
+    def get_by_id(cls, shelter_id):
+        """Get one shelter by ID."""
+        db = get_db()
+        row = db.execute("SELECT * FROM shelters WHERE shelter_id = ?", (shelter_id,)).fetchone()
+        if row is None:
+            return None
+        return cls._from_db_row(row)
+    
+    def get_tasks(self):
+        """Get all tasks that are assigned to this shelter."""
+        if not self.tasks:
+            db = get_db()
+            rows = db.execute("SELECT * FROM task WHERE shelter_id = ?", (self.id,)).fetchall()
+            for row in rows:
+                self.tasks.append(Task._from_db_row(row))        
+        return self.tasks
 
     @classmethod
     def _from_db_row(cls, row):
