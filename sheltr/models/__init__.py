ac5943b896e3f14fa733e0cced321f7ca750688c
Miguel-HernandezB <54182236+Miguel-HernandezB@users.noreply.github.com>
2025-12-07 23:12:31 -0400

    Added donations to init

--- sheltr/models/__init__.py
+++ sheltr/models/__init__.py
@@ -8,5 +8,6 @@ from .volunteer import Volunteer
 from .manager import Manager
 from .emergency import Emergency
 from .task import Task
+from .donation import Donation
 
-__all__ = ['User', 'Volunteer', 'Manager', 'Emergency', 'Task']
+__all__ = ['User', 'Volunteer', 'Manager', 'Emergency', 'Task', 'Donation']
