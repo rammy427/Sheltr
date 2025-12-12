ba0bc03cfed203b9134069d4ce9bf374a27a9b32
gaba-alemana <gabriela.german@upr.edu>
2025-12-11 22:20:41 -0400

    Finished map, fixed size, added markers for the shelters

--- sheltr/emergency.py
+++ sheltr/emergency.py
@@ -1,4 +1,4 @@
-from flask import (Blueprint, render_template, g)
+from flask import (Blueprint, render_template, render_template_string, g)
 from sheltr.models import Emergency
 from sheltr.auth import login_required
 from sheltr.db import get_db
@@ -25,13 +25,54 @@ def specific_emergency(e_id):
 
     emergency = Emergency.get_one_by_id(e_id)
     shelters = Emergency.assigned_shelters(e_id)
-    render_map()
-    return render_template('single_emergency.html', emergency = emergency, shelters = shelters)
+    map = render_map(e_id = e_id)
+    return render_template('single_emergency.html', emergency = emergency, shelters = shelters, map = map)
+
 
 @bp.route('/<int:e_id>')
 @login_required
 
-def render_map():
+def render_map(e_id):
+    """ Create the map to render in a single emergency. """
     
-    shelter_map = folium.Map(location = [18.2208, 66.5901], tiles = 'OpenStreetMap')
+    # Limiting regions
+    min_lon = -68.3469
+    max_lon = -65.1156267
+    min_lat = 17.630649
+    max_lat = 18.636163
+
+    shelter_map = folium.Map(location = [18.200178, -66.5], tiles = 'OpenStreetMap', 
+    width = '100%', height = '100%', zoom_start = 9, max_bounds = True, control_scale = True, 
+    min_lon = min_lon, max_lon = max_lon, min_lat = min_lat, max_lat = max_lat)
+
+    # Get list of shelters 
+    shelters = Emergency.assigned_shelters(e_id)
+
+    for s in shelters:
+        coords = s.location.split(',')
+
+        # Parse string
+        name = coords[0]
+        lat = float(coords[1])
+        long = float(coords[2])
+
+        # Create a marker for each shelter 
+        folium.Marker(location= [lat, long], tooltip= 'Shelter', popup= name, icon= folium.Icon(icon = 'home', color = 'persimmon')).add_to(shelter_map)
+
+
+    # Create limiting regions
+    folium.CircleMarker([max_lat, min_lon], tooltip="Upper Left Corner").add_to(shelter_map)
+    folium.CircleMarker([min_lat, min_lon], tooltip="Lower Left Corner").add_to(shelter_map)
+    folium.CircleMarker([min_lat, max_lon], tooltip="Lower Right Corner").add_to(shelter_map)
+    folium.CircleMarker([max_lat, max_lon], tooltip="Upper Right Corner").add_to(shelter_map) 
+
     shelter_map.save('sheltr/templates/shelter_map.html')
+    iframe = shelter_map.get_root()._repr_html_()
+
+    return render_template_string(
+        """ <!DOCTYPE html>
+            <html>
+                <body>
+                    {{iframe|safe}}
+                </body>
+            </html> """, iframe=iframe)
